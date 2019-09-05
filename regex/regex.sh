b6a36cd88724ad15d50826dfcb2ee5ea3f66395a
monjo003 <monjo003@morris.umn.edu>
2019-09-04 21:07:31 -0500

    Regex 0 works as expected

---
+++ regex/regex.sh
@@ -0,0 +1,12 @@
+# I used Rubular editor
+
+#Regex 0
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > outputs/r1_output.txt
+
+#Regex 1
+
+#/(?:I\sam\s)([A-Z]\w+)(?:..)(?:My\sfavorite\ssandwich\sis\s)([a-z]\w+) < r1_input.txt
+
+#Regex 2
+
+#((?:\S+)?\S*\.\S*)|(for.*)|(to.*)
