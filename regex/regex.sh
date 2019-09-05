2756bbf92e19eb7a6f2b4b6543d62aeccc7effaf
monjo003 <monjo003@morris.umn.edu>
2019-09-04 21:18:30 -0500

    copied and pasted tests.bats from the new repo

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,9 +1,11 @@
 # I used Rubular editor
 
 #Regex 0
-sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > outputs/r1_output.txt
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > outputs/r0_output.txt
 
 #Regex 1
+
+sed -E 's/\* (?:I\sam\s)([A-Z]\w+)(?:..)(?:My\sfavorite\ssandwich\sis\s)([a-z]\w+)/1. \1\n2. \2\n/' < r0_input.txt > outputs/r1_output.txt
 
 #/(?:I\sam\s)([A-Z]\w+)(?:..)(?:My\sfavorite\ssandwich\sis\s)([a-z]\w+) < r1_input.txt
 
